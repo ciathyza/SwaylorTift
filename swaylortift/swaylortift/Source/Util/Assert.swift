aa179165e02261c54049fddaa1dadf5287578dac
Sascha Balkau <ts-sascha.balkau@rakuten.com>
2019-04-10 10:00:27 +0900

    Added assert class, version up to 1.0.2

---
+++ swaylortift/swaylortift/Source/Util/Assert.swift
@@ -0,0 +1,33 @@
+//
+// Assert.swift
+// swaylortift
+//
+// Created by Balkau, Sascha on 2019-04-10.
+// Copyright (c) 2019 Ciathyza. All rights reserved.
+//
+
+import Foundation
+
+
+///
+/// Provides utility methods that can be used during development.
+///
+class Assert
+{
+	///
+	/// Asserts a specific code position as being To do. Fails the app with fatal error.
+	///
+	public static func TODO() -> Never
+	{
+		fatalError("TODO")
+	}
+	
+	
+	///
+	/// Asserts a specific code position as fatal error. Fails the app with fatal error.
+	///
+	public static func fatal() -> Never
+	{
+		fatalError("Fatal error")
+	}
+}
