1a8cf6585547907c980d47ed1d4a679286280891
Sascha Balkau <ts-sascha.balkau@rakuten.com>
2019-06-14 14:56:49 +0900

    empty space in log not if category is empty

--- swaylortift/swaylortift/Source/Util/Log.swift
+++ swaylortift/swaylortift/Source/Util/Log.swift
@@ -213,7 +213,8 @@ public struct Log
 	{
 		let prefix = modePrefix(Date(), file: file, function: function, line: line)
 		let itemsString = items.map { "\($0)" }.joined(separator: Log.separator)
-		let line = "\(Log.prompt)\(Log.getLogLevelName(logLevel: logLevel))\(category) \(prefix)\(itemsString)"
+		let cat = category.isEmpty ? category : category + String.Space
+		let line = "\(Log.prompt)\(Log.getLogLevelName(logLevel: logLevel))\(cat)\(prefix)\(itemsString)"
 		Swift.print(line, terminator: Log.terminator)
 		
 		if let logFilePath = Log.logFilePath
